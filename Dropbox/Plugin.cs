541b927ec79fdc3fb5207d4436ef4533beaf8c3f
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-09-11 02:01:24 -0400

    initial

---
+++ Dropbox/Plugin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Dropbox.Configuration;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+
+namespace Dropbox
+{
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    {
+        public static Plugin Instance { get; private set; }
+
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+            : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+
+        private Guid _id = new Guid("830fc68f-b964-4d2f-b139-48e22cd143c7");
+        public override Guid Id
+        {
+            get { return _id; }
+        }
+
+        public IEnumerable<PluginPageInfo> GetPages()
+        {
+            return new[]
+            {
+                new PluginPageInfo
+                {
+                    Name = Name,
+                    EmbeddedResourcePath = GetType().Namespace + ".Configuration.configPage.html"
+                }
+            };
+        }
+
+        public override string Name
+        {
+            get { return Constants.Name; }
+        }
+
+        public override string Description
+        {
+            get { return Constants.Description; }
+        }
+    }
+}
